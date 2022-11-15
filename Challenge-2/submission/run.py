b71ff7f6c91bbdb18b7f4b9a2dfc88672f2b7076
Andrzej Szablewski <45316794+TheRootOf3@users.noreply.github.com>
2022-11-15 03:18:57 +0000

    Challenge-2: Changed test set file format interface to npz (#5)
    * Challenge-2: Changed test set file format interface to npz
    
    * Give the code a quick format with black!
    
    Co-authored-by: Jeremy <jeremyloyingping@googlemail.com>

--- Challenge-2/submission/run.py
+++ Challenge-2/submission/run.py
@@ -1,26 +1,31 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import tensorflow as tf
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
 import numpy as np
+import tensorflow as tf
 
 
 def main():
-    # load TF model
-    model = tf.keras.models.load_model('model')
+    # load the Tensorflow model
+    model = tf.keras.models.load_model("model")
 
+    # synchronise with DOXA
     print("READY")
-    # read and load a test set from stdin
-    test_set_path = input()
-    x_test = np.load(test_set_path)
 
-    # run model on the test set
-    y_preds = model.predict(x_test, verbose=0)
-    y_pred_classes = tf.math.argmax(y_preds, axis=1)
-    
-    # write predicted classes to stdout
-    for pred in y_pred_classes:
-        print(int(pred))
-    
+    # read the test set file path from stdin
+    test_set_path = input()
+
+    # load the test set
+    x_test = np.load(test_set_path)["data"]
+
+    # run the model on the test set
+    predictions = tf.math.argmax(model.predict(x_test, verbose=0), axis=1)
+
+    # write the class predictions to stdout
+    for prediction in predictions:
+        print(int(prediction))
+
+
 if __name__ == "__main__":
     main()
